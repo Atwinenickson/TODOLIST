cd941129a0c3ab1e5a54df6b6d1d8638d6825492
Atwinenickson <atwiinenicxon@gmail.com>
2022-09-02 00:45:33 +0300

    import connection and routes in the app.js file

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
-const express = require('express');
+var express = require('express');
+var bodyParser = require('body-parser');
 
-const app = express();
-const PORT = 3000;
+var connection = require('./app/config/connection');
+var routes = require('./app/controllers/routes');
 
-app.get('/', (req, res)=>{
-	res.status(200);
-	res.send("Welcome to root URL of Server");
+var app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+connection.init();
+routes.configure(app);
+
+var server = app.listen(8000, function(){
+  console.log('Server listening on port ' + server.address().port);
 });
-
-app.listen(PORT, (error) =>{
-	if(!error)
-		console.log("Server is Successfully Running and App is listening on port "+ PORT)
-	else
-		console.log("Error occurred, server can't start", error);
-	}
-);
